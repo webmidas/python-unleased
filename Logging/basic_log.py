6cedbfe747d670d575903fbb48f13b4a58333fed
Ashish Jha <ajlti2019@gmail.com>
2023-02-12 16:03:04 +0530

    Adding python logger codes

---
+++ Logging/basic_log.py
@@ -0,0 +1,7 @@
+import logging
+
+FORMAT = '%(asctime)s %(clientip)-15s %(user)-8s %(message)s'
+logging.basicConfig(format=FORMAT)
+d = {'clientip': '192.168.0.1', 'user': 'fbloggs'}
+logger = logging.getLogger('tcpserver')
+logger.warning('Protocol problem: %s', 'connection reset', extra=d)
